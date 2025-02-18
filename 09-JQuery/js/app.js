3e4c6d6b060349e7e4de54551ab8784090fda1c1
FernanLeandro <fernanflorez4@gmail.com>
2025-02-18 16:54:48 -0500

    se modifica css en general y js de todolist

--- 09-JQuery/js/app.js
+++ 09-JQuery/js/app.js
@@ -1,5 +1,9 @@
-// Add Task
 $(function(){
+    // Count Task
+    countTasks()
+    // Conunt Remains
+    countRemains()
+    // Add Task
     $('footer').on('click', '#add', function(){
         if($('#input-task').val().length > 0) {
 
@@ -10,18 +14,29 @@ $(function(){
                     </article>'
             $('section.list').append($task)
             $('#input-task').val('')
-
+            countTasks()
+            countRemains()
         } else {
             alert('Please! Enter a Task')
         }
     })
     // Toggle Task (Remain/Done)
-    $('article').on('click', 'input[type=checkbox]', function() {
+    $('body').on('click', 'input[type=checkbox]', function() {
         // if cheched
         if($(this).prop('checked')) {
         $(this).parent().addClass('checked') 
         } else {
         $(this).parent().removeClass('checked')
         }
+        countRemains()
     })
 })
+function countTasks() {
+    $('.number-tasks').text($('article').length)
+    $('.title-tasks').text($('article').length > 1 ? 'Tasks' : 'Task')
+}
+// Count Remains
+function countRemains() {
+    $('.number-remains').text(Math.abs($('.checked').length - $('article').length))
+    $('.title-remains').text($('.checked').length > 1 ? 'Remains' : 'Remain')
+}
