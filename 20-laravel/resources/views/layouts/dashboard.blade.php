29700934d595045a4c2d075a135763552607ab0e
FernanLeandro <fernanflorez4@gmail.com>
2025-11-13 11:39:08 -0500

    perfil, dashboard, restaurar contraseña

--- 20-laravel/resources/views/layouts/dashboard.blade.php
+++ 20-laravel/resources/views/layouts/dashboard.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,18 @@
     <!-- Tailwind CDN for quick styling improvements (keeps functionality intact) -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="min-h-[100dvh] bg-[url({{ asset('images/pets.jpg') }})] bg-contain bg-cover bg-center bg-black w-full flex flex-col gap-4 items-center justify-center p-8">
+@php
+if(Auth::user()->role == 'Administrador') {
+$image = 'images/pets-dashboard-2.png';
+} else {
+$image = 'images/pets-dashboard.png';
+}
+@endphp
+
+<body class="min-h-[100dvh] bg-[url({{ asset($image) }})] bg-contain bg-cover bg-fixed bg-center bg-black w-full flex flex-col gap-4 items-center justify-center p-8 pt-20">
+    @include('layouts.navbar')
     @yield('content')
+    @yield('js')
 </body>
+
 </html>
