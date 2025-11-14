6ff701c24730533285fc32e773aa5117022a8b96
FernanLeandro <fernanflorez4@gmail.com>
2025-11-14 17:12:35 -0500

    dashboard

--- 20-laravel/resources/views/dashboard.blade.php
+++ 20-laravel/resources/views/dashboard.blade.php
@@ -1,19 +1,197 @@
 @extends('layouts.dashboard')
-@section('title', 'Dashboard ADMIN: Larapets 🐶')
+@section('title', 'dashboard ADMIN: Larapets')
 
 @section('content')
 
- <h1>Dashboard</h1>
- <h2>{{ Auth::user()->fullname }}</h2>
- <h3>You're logged in!</h3>
- <form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <a href="javascript:;"
-            onclick="event.preventDefault();
-            this.closest('form').submit();">
-        log Out
-    </a>
+    <h1 class="text-4xl text-white flex gap-2 items-center justify-center pb-4 border-b-2 border-neutral-50 mb-10">
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-10" fill="currentColor" viewBox="0 0 256 256"><path d="M224,40V216a8,8,0,0,1-16,0V40a8,8,0,0,1,16,0ZM192,64v40a16,16,0,0,1-16,16H80a16,16,0,0,1-16-16V64A16,16,0,0,1,80,48h96A16,16,0,0,1,192,64Zm-16,0H80v40h96Zm16,88v40a16,16,0,0,1-16,16H40a16,16,0,0,1-16-16V152a16,16,0,0,1,16-16H176A16,16,0,0,1,192,152Zm-16,0H40v40H176Z"></path></svg>
+        Dashboard
+    </h1>
 
- </form>
+    <!-- Cards -->
+<div class="flex flex-wrap gap-4 items-center justify-center">
+    {{-- module user --}}
+  <div  class="card text-white bg-[#0006] w-96 shadow-sm">
+  <figure class="h-48 overflow-hidden">
+    <img class="w-full h-full object-cover" src="images/model-user.png" alt="">
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Module Users</h2>
+    {{-- stats --}}
+    <ul class="list bg-[#0003] rounded-box shadow-md">
+  
+  <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Stadistics:</li>
+  
+  <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Total users:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\User::count() }}
+     </button>
+  </li>
 
- @endsection
+    <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Customer:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\User::where('role', 'customer')->count() }}
+     </button>
+  </li>
+
+    <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Active</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\User::where('active', 1)->count() }}
+     </button>
+  </li>
+</ul>
+    <div class="card-actions justify-end">
+        <a class="btn btn-outline hover:bg-[#fff6] hover:text-white mt-3" href="{{ url('users') }}">Enter
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg>
+        </a>
+    </div>
+  </div>
+</div>
+
+    <div class="card text-white bg-[#0006] w-96 shadow-sm">
+  <figure class="h-48 overflow-hidden">
+    <img class="w-full h-full object-cover" src="images/model-pets.png" alt="">
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Module Pets</h2>
+    {{-- stats --}}
+    <ul class="list bg-[#0003] rounded-box shadow-md">
+  
+  <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Stadistics:</li>
+  
+  <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Total Pets:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\Pet::count() }}
+     </button>
+  </li>
+
+    <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Dogs:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\Pet::where('kind', 'dog')->count() }}
+     </button>
+  </li>
+
+    <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Cats:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\Pet::where('kind', 'cat')->count() }}
+     </button>
+  </li>
+</ul>
+    <div class="card-actions justify-end">
+        <a class="btn btn-outline hover:bg-[#fff6] hover:text-white mt-3" href="{{ url('users') }}">Enter
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg>
+        </a>
+    </div>
+  </div>
+</div>
+
+  <div class="card text-white bg-[#0006] w-96 shadow-sm">
+  <figure class="h-48 overflow-hidden">
+    <img src="images/adoption-pets.png" class="w-full h-full object-cover" alt="">
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Module Adoptions</h2>
+    {{-- stats --}}
+    <ul class="list bg-[#0003] rounded-box shadow-md">
+      
+  <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Stadistics:</li>
+  
+  <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Total Adoptions:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\Adoption::count() }}
+     </button>
+  </li>
+
+    <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Dog Adopted:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\Pet::where('status', 1)->where('kind', 'dog')->count() }}
+     </button>
+  </li>
+
+    <li class="list-row">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-8" fill="currentColor" viewBox="0 0 256 256">
+                            <path
+                                d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z">
+                            </path>
+                        </svg>
+    <div class="flex items-center">
+      <div class="text-xs uppercase font-semibold opacity-60">Cat Adopted:</div>
+    </div>
+     <button class="btn btn-square btn-ghost">
+        {{ App\Models\Pet::where('status', 1)->where('kind', 'cat')->count() }}
+     </button>
+  </li>
+</ul>
+    <div class="card-actions justify-end">
+        <a class="btn btn-outline hover:bg-[#fff6] hover:text-white mt-3" href="{{ url('users') }}">Enter
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="currentColor" viewBox="0 0 256 256"><path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path></svg>
+        </a>
+    </div>
+  </div>
+</div>
+</div>
+@endsection
--- 20-laravel/resources/views/layouts/navbar.blade.php
+++ 20-laravel/resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
-<div class="navbar bg-[#000a] text-white shadow-sm sahdow-sm fixed top-0">
+<div class="navbar bg-[#000a] text-white shadow-sm sahdow-sm fixed z-50 top-0">
     <div class="navbar-start">
-        <div class="dropdown">
+        <div class="dropdown mt-5">
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
@@ -26,6 +26,10 @@
                         Dashboard
                     </a>
                 </li>
+
+                {{-- Role: Administrator --}}
+                @if(Auth::user()->role == 'Administrador')
+
                 <li>
                     <a href="{{ url('users/') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="size-4" fill="currentColor" viewBox="0 0 256 256">
@@ -50,10 +54,36 @@
                         Module Adoptions
                     </a>
                 </li>
+                @else
+                <li>
+                    <a href="{{ url('myprofile/') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-4" fill="currentColor" viewBox="0 0 256 256">
+                            <path d="M223,57a58.07,58.07,0,0,0-81.92-.1L128,69.05,114.91,56.86A58,58,0,0,0,33,139l89.35,90.66a8,8,0,0,0,11.4,0L223,139a58,58,0,0,0,0-82Zm-11.35,70.76L128,212.6,44.3,127.68a42,42,0,0,1,59.4-59.4l.2.2,18.65,17.35a8,8,0,0,0,10.9,0L152.1,68.48l.2-.2a42,42,0,1,1,59.36,59.44Z"></path>
+                        </svg>
+                        My Profile
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ url('makedoptions/') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-4" fill="currentColor"" viewBox=" 0 0 256 256">
+                            <path d="M223,57a58.07,58.07,0,0,0-81.92-.1L128,69.05,114.91,56.86A58,58,0,0,0,33,139l89.35,90.66a8,8,0,0,0,11.4,0L223,139a58,58,0,0,0,0-82Zm-11.35,70.76L128,212.6,44.3,127.68a42,42,0,0,1,59.4-59.4l.2.2,18.65,17.35a8,8,0,0,0,10.9,0L152.1,68.48l.2-.2a42,42,0,1,1,59.36,59.44Z"></path>
+                        </svg>
+                        Make Adoptions
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ url('myadoptions/') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-4" fill="currentColor"" viewBox=" 0 0 256 256">
+                            <path d="M223,57a58.07,58.07,0,0,0-81.92-.1L128,69.05,114.91,56.86A58,58,0,0,0,33,139l89.35,90.66a8,8,0,0,0,11.4,0L223,139a58,58,0,0,0,0-82Zm-11.35,70.76L128,212.6,44.3,127.68a42,42,0,0,1,59.4-59.4l.2.2,18.65,17.35a8,8,0,0,0,10.9,0L152.1,68.48l.2-.2a42,42,0,1,1,59.36,59.44Z"></path>
+                        </svg>
+                        My Adoptions
+                    </a>
+                </li>
+                @endif
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <a href="javascript:;" class="flex gap-2"
+                        <a href="javascript:;" class="flex gap-2 items-center justify-center"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                             <svg xmlns="http://www.w3.org/2000/svg" class="size-4" fill="currentColor" viewBox="0 0 256 256">
